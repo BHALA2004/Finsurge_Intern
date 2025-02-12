ec2f771f45a209a0e0fb9aaea506a0fe118f9a92
BHALA2004 <balakannantvl@gmail.com>
2025-02-12 17:00:47 +0530

    changes

---
+++ src/com/clonable/Main.java
@@ -0,0 +1,36 @@
+package com.clonable;
+
+class Address {
+    String city;
+    Address(String city) {
+        this.city = city;
+    }
+}
+
+class Person implements Cloneable {
+    String name;
+    Address address;
+
+    Person(String name, String city) {
+        this.name = name;
+        this.address = new Address(city);
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone(); // Shallow copy
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Person p1 = new Person("Bob", "New York");
+        Person p2 = (Person) p1.clone();  // Cloning
+
+        p2.address.city = "Los Angeles";  // Changing p2's city also affects p1
+
+        System.out.println(p1.name + " - " + p1.address.city);  // ❌ "Los Angeles"
+        System.out.println(p2.name + " - " + p2.address.city);
+    }
+}
+
+
---
+++ src/com/comparator/ComparatorImplement.java
@@ -0,0 +1,39 @@
+package com.comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class ComparatorImplement implements Comparator<Students> {
+   public int compare(Students s1,Students s2){
+       return s1.studentName.compareTo(s2.studentName);
+   }
+}
+class Students{
+    String studentName;
+
+    public Students(String studentName) {
+        this.studentName = studentName;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Students s1 = new Students("Bala");
+        Students s2 = new Students("Danush");
+        Students s3 = new Students("Asia");
+        List<Students> studentsList = new ArrayList<>();
+        studentsList.add(s1);
+        studentsList.add(s2);
+        studentsList.add(s3);
+        Collections.sort(studentsList, new ComparatorImplement());
+        for (Students s  : studentsList){
+            System.out.println((s.studentName));
+        }
+            
+
+
+
+
+    }
+}
