a7bc43f53f76d914c04a706d495592dafb3190cb
BHALA2004 <balakannantvl@gmail.com>
2025-02-21 15:24:52 +0530

    Initial commit

--- src/day12/files/task/ListAllFiles.java
+++ src/day12/files/task/ListAllFiles.java
@@ -10,15 +10,66 @@ public class ListAllFiles {
         String path = scanner.next();
         File file = new File(path);
 
+        System.out.println("1.Print Both File and Directory\n2.Print File Only\n3.Print Directory Only\n4.Print PDF Formated File\n5.Print WORD Formated File\n6.Exit");
+
         if (file.exists() && file.isDirectory()) {
-            listFilesRecursively(file, 0);
+            int option;
+            while (true){
+                System.out.println();
+                System.err.println("Enter Option :");
+                while (true){
+                    try {
+                        option = scanner.nextInt();
+                        if(option>=1 && option<=6){
+                            break;
+                        }
+                        else {
+                            System.out.println("Invalid,Enter Valid:");
+                        }
+                    }
+                    catch (Exception e){
+                        System.out.println("Error Occured,Enter Valid:");
+                        scanner.next();
+                    }
+                }
+                switch (option){
+                    case 1:
+                        listFilesRecursivelyForFileAndDirectory(file,0);
+                        break;
+                    case 2:
+                        listFilesRecursivelyForFile(file,0);
+                        break;
+                    case 3:
+                        listFilesRecursivelyForDiectory(file,0);
+                        break;
+                    case 4:
+                        listFilesRecursivelyForPDF(file,0);
+                        if(i==0){
+                            System.out.println("No Pdf Found");
+                        }
+                        break;
+                    case 5:
+                        listFilesRecursivelyForWord(file,0);
+                        if(j==0){
+                            System.out.println("No Word is Found");
+                        }
+                        break;
+                    case 6:
+                        System.exit(0);
+                    case 7:
+                        System.out.println("Enter Valid : ");
+                        break;
+                }
+            }
+
+
         } else {
-            System.out.println("Path not found or is not a directory.");
+            System.err.println("Result = Path not found or is not a directory.");
         }
         scanner.close();
     }
 
-    public static void listFilesRecursively(File directory, int level) {
+    public static void listFilesRecursivelyForFileAndDirectory(File directory, int level) {
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
@@ -26,14 +77,78 @@ public class ListAllFiles {
                     System.out.println("File = "+file.getName());
                 }
                 else {
-                    System.out.println("Directory = "+file.getName());
+                    System.out.println("    Directory = "+file.getName());
                 }
                 if (file.isDirectory()) {
-                    listFilesRecursively(file, level + 1);
+                    listFilesRecursivelyForFileAndDirectory(file, level + 1);
                 }
             }
         }
     }
+
+    public static void listFilesRecursivelyForDiectory(File directory, int level) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if(file.isDirectory()){
+                    System.out.println("File = "+file.getName());
+                }
+                if (file.isDirectory()) {
+                    listFilesRecursivelyForDiectory(file, level + 1);
+                }
+            }
+        }
+    }
+
+    public static void listFilesRecursivelyForFile(File directory, int level) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if(file.isFile()){
+                    System.out.println("File = "+file.getName());
+                }
+                if (file.isDirectory()) {
+                    listFilesRecursivelyForFile(file, level + 1);
+                }
+            }
+        }
+    }
+    static int i = 0;
+    public static void listFilesRecursivelyForPDF(File directory, int level) {
+        File[] files = directory.listFiles();boolean flag = true;
+        if (files != null) {
+            for (File file : files) {
+                if(file.isFile() && file.getName().toLowerCase().endsWith(".pdf")){
+                    System.out.println("File = "+file.getName());
+                    i++;
+                }
+                if (file.isDirectory()) {
+                    listFilesRecursivelyForPDF(file,level+1);
+                }
+            }
+        }
+    }
+
+    static int j = 0;
+
+    public static void listFilesRecursivelyForWord(File directory, int level) {
+        File[] files = directory.listFiles();boolean flag = true;
+        if (files != null) {
+            for (File file : files) {
+                if(file.isFile() && file.getName().toLowerCase().endsWith(".docx")){
+                    System.out.println("File = "+file.getName());
+                    flag = false;
+                    j++;
+                }
+                if (file.isDirectory()) {
+                    listFilesRecursivelyForWord(file, level + 1);
+                }
+            }
+        }
+
+    }
+
+
 
 //    private static void printIndented(File file, int level) {
 //        String indent = "  ".repeat(level);
