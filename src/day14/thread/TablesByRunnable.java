73c62e70d0bd85f1df76893d1490aef1a1d72deb
BHALA2004 <balakannantvl@gmail.com>
2025-02-25 13:14:45 +0530

    Initial commit

--- src/day14/thread/TablesByRunnable.java
+++ src/day14/thread/TablesByRunnable.java
@@ -1,21 +1,27 @@
 package day14.thread;
 
 
+import java.util.Scanner;
+
 class TableRun implements Runnable{
     int number;
-    public TableRun(int number){
+    int table;
+    public TableRun(int number,int table){
         this.number=number;
+        this.table=table;
     }
 
     public void run(){
-        System.out.println(Thread.currentThread().getName()+" "+number*2);
+        System.out.println(Thread.currentThread().getName()+" "+number*table);
     }
 }
 public class TablesByRunnable {
     public static void main(String[] args) {
-
+        System.out.println("Enter which table:");
+        Scanner scanner = new Scanner(System.in);
+        int table = scanner.nextInt();
        for (int i = 1;i<=20;i++){
-           TableRun tableRun = new TableRun(i);
+           TableRun tableRun = new TableRun(i,table);
            Thread thread = new Thread(tableRun);
            thread.start();
        }
